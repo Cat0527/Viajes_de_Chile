1b64125fafc7f277e0b964910719b57c9220fe4e
cat0527 <caterin.mendez0527@gmail.com>
2024-07-22 17:05:13 -0400

    jquery-js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,15 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+//jquery
 
 $(document).ready(function(){
     $("p").dblclick(function(){
         $(this).hide("slow");
     });
-    $(document).ready(function(){
-        $("img").click(function(){
-            $(this).show(); /*Queria lograr el efecto de clikear la imagen para que apareciera el texto pero no me resulto como queria*/
-        });
-    });
 });
 
+// Selector para elementos con clase "textD"
 $(document).ready(function() {
-    // Selector para elementos con clase "textD"
     $(".textD").click(function() {
         $(this).css({
         "color": "#BA23DB",
@@ -30,3 +26,27 @@ $(document).ready(function() {
         });
     });
 });
+
+//smooth scroll
+$(document).ready(function() {
+    $('a').click(function(event) {
+      event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+      const target = $(this).attr('id'); // Obtener el ID del destino (e.g., "#inicio")
+        
+        $('html, body').animate({
+        scrollTop: $(target).offset().top // Desplazarse a la posición superior del destino
+      }, 1500); // Duración de la animación (en milisegundos)
+    });
+});
+
+
+//Link iconos footer js
+
+const iconosFF = document.querySelectorAll('.iconoFF');
+iconosFF.forEach(icono => {
+    icono.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+    window.location.href = this.querySelector('a').href;
+    });
+});
+
